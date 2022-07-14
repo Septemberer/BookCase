5b79a56a91a181b81b040463804556ef446d0d3b
Septemberer <tolik.laptev.02@mail.ru>
2022-07-14 13:37:09 +0300

    Почти все

--- src/api/book/book.controller.ts
+++ src/api/book/book.controller.ts
@@ -4,17 +4,4 @@ import { Book } from './book.entity';
 import { BookService } from './book.service';
 
 @Controller('book')
-export class BookController {
-  @Inject(BookService)
-  private readonly service: BookService;
-
-  @Get(':id')
-  public getBook(@Param('id', ParseIntPipe) id: number): Promise<Book> {
-    return this.service.getBook(id);
-  }
-
-  @Post()
-  public createBook(@Body() body: CreateBookDto): Promise<Book> {
-    return this.service.createBook(body);
-  }
-}
+export class BookController {}
--- src/api/user/user.service.ts
+++ src/api/user/user.service.ts
@@ -1,20 +1,25 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository, UpdateResult } from 'typeorm';
+import { CreateBookDto } from '../book/book.dto';
+import { Book } from '../book/book.entity';
 import { CreateUserDto } from './user.dto';
 import { User } from './user.entity';
 
 @Injectable()
 export class UserService {
   @InjectRepository(User)
-  private readonly repository: Repository<User>;
+  private readonly userRepository: Repository<User>;
 
-  public getUser(id: number): Promise<User> { // Получение юзера (и списка книг) (5)
-    return this.repository.findOne(id);
-  }
+  @InjectRepository(Book)
+  private readonly bookRepository: Repository<Book>;
 
-  public deleteUser(id: number): void { // Удаление юзера (3.1)
-    this.repository.delete(id);
+  public createUser(body: CreateUserDto): Promise<User> { // Добавление пользователя (1)
+    const user: User = new User();
+
+    user.name = body.name;
+
+    return this.userRepository.save(user);
   }
 
   public updateUser(id: number, body: CreateUserDto): Promise<User> { // Редактирование пользователя (2)
@@ -22,15 +27,45 @@ export class UserService {
 
     user.name = body.name;
 
-    this.repository.delete(id);
-    return this.repository.save(user);
+    this.userRepository.delete(id);
+    return this.userRepository.save(user);
   }
 
-  public createUser(body: CreateUserDto): Promise<User> { // Добавление пользователя (1)
-    const user: User = new User();
+  public deleteUser(id: number): void { // Удаление юзера (3.1)
+    this.userRepository.delete(id);
+  }
 
-    user.name = body.name;
+  public async addAbonement(id: number): Promise<void> { // Добавление абонемента, если нету (3.2)
+    const user = await this.userRepository.findOne(id);
+    this.userRepository
+      .createQueryBuilder("user")
+      .update(user)
+      .set({ abonement: true })
+      .where("user.id = :id", { id: id })
+      .andWhere("user.abonement = :abonement", { abonement: false })
+      .execute();
+  }
 
-    return this.repository.save(user);
+  public getAllUsers(): Promise<User[]> { // Получение всех юзеров (4)
+    return this.userRepository
+      .createQueryBuilder("user")
+      .getMany();
+  }
+
+  public getUser(id: number): Promise<User> { // Получение юзера (и списка книг) (5)
+    return this.userRepository.findOne(id);
+  }
+
+  public createBook(body: CreateBookDto): Promise<Book> { // Создать книгу (6)
+    const book: Book = new Book();
+
+    book.name = body.name;
+
+    return this.bookRepository.save(book);
+  }
+
+
+  public getBook(id: number): Promise<Book> { // Получить книгу (9)
+    return this.bookRepository.findOne(id);
   }
 }
